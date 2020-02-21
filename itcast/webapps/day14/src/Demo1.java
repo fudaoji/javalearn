59ea21596fd64fd584ecb53bdd6aae2ba897832a
fdj <461960962@qq.com>
2020-02-21 21:55:57 +0800

    add some modules

---
+++ itcast/webapps/day14/src/Demo1.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet("/demo1")
+public class Demo1 extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("hello");
+    }
+}
--- itcast/webapps/web/index.jsp
+++ itcast/webapps/web/index.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: fudaoji
   Date: 2020/2/19
-  Time: 下午10:50
+  Time: 下午10:45
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
